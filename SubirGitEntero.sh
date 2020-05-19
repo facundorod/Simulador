7f910e66183fab9b8ac3519131598b2fc982f939
Facundo Hernán Rodríguez <farodriguez@alumnos.exa.unicen.edu.ar>
2020-05-19 20:15:35 -0300

    CRUD Arrythmia V2

--- SubirGitEntero.sh
+++ SubirGitEntero.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-git add .
-
-git commit -m "$1"
-
-git push origin master
+if [$# -eq 0]; then
+	echo "Debe especificar un mensaje para el commit" >&2
+else 
+	git add .
+	git commit -m "$1"
+	git push origin master
+fi
 
