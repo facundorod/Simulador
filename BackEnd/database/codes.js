6d42ac2fe0ed367422174463df06925a7c6d2b5e
Facundo Hernán Rodríguez <farodriguez@alumnos.exa.unicen.edu.ar>
2020-12-29 22:58:50 -0300

    Move backend to another repository, changes on sidebar and abm pages

--- BackEnd/database/codes.js
+++
@@ -1,9 +0,0 @@
-// Códigos de errores en consultas en PostgreSQL.
-
-const UNIQUE_VIOLATION = 23505;
-const FOREIGN_KEY_VIOLATION = 23503;
-
-module.exports = {
-    UNIQUE_VIOLATION,
-    FOREIGN_KEY_VIOLATION
-}
